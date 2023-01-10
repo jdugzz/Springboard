be923b9cb5b6c06540ee67193496fc13677baa3b
jdugzz <97847249+jdugzz@users.noreply.github.com>
2023-01-09 20:03:13 -0500

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -1,19 +1,9 @@
-/** Connect Four
- *
- * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
- * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
- */
 
 const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 'one'; // active player: 1 or 2
 const board = []; // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
 
 function makeBoard() {
   for (let i = 0; i < HEIGHT; i++) {// for loop that runs until board array has 
@@ -23,9 +13,7 @@ function makeBoard() {
 }
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.getElementById('board');
-  // TODO: add comment for this code
   const top = document.createElement('tr'); // create table row element
   top.setAttribute('id', 'column-top'); // set id of element to be 'column-top'
   top.addEventListener('click', handleClick); // add event listener so that event is triggered on click
@@ -56,8 +44,6 @@ function findSpotForCol(x) {
   }
 }
 
-/** placeInTable: update DOM to place piece into HTML table of board */
-
 function placeInTable(y, x) {
   const piece = document.createElement('div');
   piece.setAttribute('class', 'piece');
@@ -65,13 +51,9 @@ function placeInTable(y, x) {
   document.getElementById(`${y}-${x}`).append(piece);
 }
 
-/** endGame: announce game end */
-
 function endGame(msg) {
   alert(msg);
 }
-
-
 
 function checkForTie() {
   let topRow = board[0];
@@ -80,41 +62,24 @@ function checkForTie() {
   }
 }
 
-/** handleClick: handle click of column top to play piece */
-
 function handleClick(evt) {
-  // get x from ID of clicked cell
   const x = +evt.target.id;
 
-  // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   placeInTable(y, x);
 
-  // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
 
   checkForTie(board);
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  // checkForTie();
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
+
   currPlayer = playerCheck(currPlayer);
 }
 
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
   function win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
-    return cells.every(
+     return cells.every(
       ([y, x]) =>
         y >= 0 &&
         y < HEIGHT &&
@@ -123,8 +88,6 @@ function checkForWin() {
         board[y][x] === currPlayer
     );
   }
-
-  // TODO: read and understand this code. Add comments to help you.
 
   for (let y = 0; y < HEIGHT; y++) { // runs a for loop on every y coord
     for (let x = 0; x < WIDTH; x++) { // runs a for loop on every x coord
