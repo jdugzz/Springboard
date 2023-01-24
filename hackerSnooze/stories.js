8ee1182e24cdb74762fd81b7e85db95a2fd52b7e
jdugzz <97847249+jdugzz@users.noreply.github.com>
2023-01-24 18:43:20 -0500

    Create stories.js

---
+++ hackerSnooze/stories.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let storyList;
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+  putStoriesOnPage();
+}
+
+function generateStoryMarkup(story) {
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <span class='icon' id=heart>&#9734</span>
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
