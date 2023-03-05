20c0dd9a490e6b74ddbacb7b26be789599caebf1
jdugzz <97847249+jdugzz@users.noreply.github.com>
2023-03-05 10:21:19 -0500

    Update app.py

--- sqlAlchemy/app.py
+++ sqlAlchemy/app.py
@@ -1,18 +1,28 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, flash
 from sqlalchemy import text
-from models import User, db, connect_db
+from models import User, Post, db, connect_db
 from unittest import TestCase
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///Users'
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'asdfasdfasdf'
 
 connect_db(app)
+
+def add_starter():
+    """simply just add a single individual to db"""
+    with app.app_context():
+        db.create_all()
+        richard = User(first_name='Richard', last_name='Sherman')
+        db.session.add(richard)
+        db.session.commit()
 
 @app.route('/')
 def home():
     """essentially the homepage"""
-    return redirect('/users')
+    posts = Post.query.limit(5).all()
+    return render_template('homepage.html', posts=posts)
 
 @app.route('/users')
 def users():
@@ -34,16 +44,26 @@ def handle_user_add():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     img_url = request.form['img_url']
-    new_user = User(first_name=first_name, last_name=last_name, image_url=img_url)
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect('/users')
+    error = None
+    if not first_name:
+        error = 'First Name is a required field.'
+    if not last_name:
+        error = 'Last Name is a required field.'
+    if error is None:
+        new_user = User(first_name=first_name, last_name=last_name, image_url=img_url)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/users')
+    else:
+        flash(error)
+        return render_template('adduser.html')
 
 @app.route('/users/<user_id>')
 def user_info(user_id):
     """show the user information page"""
     user = User.query.get(user_id)
-    return render_template('user.html', user=user)
+    posts = Post.query.filter_by(poster_id=user.id).all()
+    return render_template('user.html', user=user, posts=posts)
 
 @app.route('/users/<user_id>/edit')
 def user_info_(user_id):
@@ -63,8 +83,59 @@ def user_info_edit(user_id):
 
 @app.route('/users/<user_id>/delete', methods=["POST"])
 def delete_user(user_id):
-    """remove a user from the database"""
     user = User.query.filter_by(id=user_id).first()
     db.session.delete(user)
+    db.session.commit()
+    return redirect('/users')
+
+@app.route('/users/<user_id>/posts/new')
+def new_post(user_id):
+    user = User.query.get(user_id)
+    return render_template('newpost.html', user=user)
+
+@app.route('/users/<user_id>/posts/new', methods=["POST"])
+def handle_new_post(user_id):
+    user = User.query.get(user_id)
+    title = request.form['title']
+    content = request.form['content']
+    error = None
+    if not title:
+        error = 'A title is required.'
+    if not content:
+        error = 'Your post must have content.'
+    if error is None:
+        new_post = Post(title=title, content=content, poster_id=user.id)
+        db.session.add(new_post)
+        db.session.commit()
+        posts = Post.query.filter_by(poster_id=user.id).all()
+        return render_template("user.html", user=user, posts=posts)
+    else: 
+        flash(error)
+        return render_template('newpost.html', user=user)
+
+@app.route('/posts/<post_id>')
+def show_post(post_id):
+    post = Post.query.get(post_id)
+    user = User.query.get(post.poster_id)
+    return render_template('posts.html', post=post, user=user)
+
+@app.route('/<post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    post = Post.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/users')
+
+@app.route('/posts/posts/<post_id>/edit')
+def show_edit_page(post_id):
+    post = Post.query.get(post_id)
+    return render_template('edit.html', post=post)
+
+@app.route('/posts/<post_id>/edit', methods=['POST'])
+def handle_edit(post_id):
+    post = Post.query.get(post_id)
+    error = None
+    post.title = request.form['title']
+    post.content = request.form['content']
     db.session.commit()
     return redirect('/users')
