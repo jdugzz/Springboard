a159db5a9e9cf80b2fb37323e67156cee9823dea
jdugzz <97847249+jdugzz@users.noreply.github.com>
2023-03-05 10:21:35 -0500

    Update models.py

--- sqlAlchemy/models.py
+++ sqlAlchemy/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime 
 
 db = SQLAlchemy() 
 
@@ -16,5 +17,21 @@ class User(db.Model):
                   nullable = False)
     last_name = db.Column(db.String(25),
                   nullable = False)
-    image_url = db.Column(db.String(200),
+    image_url = db.Column(db.String,
                    nullable = True)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                  primary_key = True,
+                  autoincrement = True)
+    title = db.Column(db.String,
+                      nullable = False)
+    content = db.Column(db.String,
+                        nullable = False)
+    created_at = db.Column(db.DateTime, 
+                           default = datetime.now)
+    poster_id = db.Column(db.Integer,
+                       db.ForeignKey('users.id'))
+    poster = db.relationship('User', backref='posts')
